02fdbbba4bda7c3195545a926a1b166679407ce3
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-12-10 20:18:52 +0200

    Update CSCB112_HM1_FNumber.cpp

--- C++_Basics/CSCB112_HM1_FNumber.cpp
+++ C++_Basics/CSCB112_HM1_FNumber.cpp
@@ -3,22 +3,18 @@
 int main()
 {
 
-    // Задача 1. Съставете С++ конзолна програма, чрез която да изведете трите си имена на екрана.
     std::cout << "Ex. 1 \n";
     std::cout << "Kolio Stoyanov Kolev \n";
 
-    //Задача 2. Съставете С++ конзолна програма, чрез която да изведете на екрана, в кой университет учите и по каква програма се обучавате.
     std::cout << "Ex.2 \n";
     std::cout << "I'm currently studying at NBU university.";
     std::cout << "\n";
     std::cout << "I'm studying informatics.  \n";
 
-    //Задача 3. Съставете С++ конзолна програма, чрез която да изведете на екрана всички служебни думи на езика С++.
     std::cout << "Ex.3 \n";
     std::cout << "AUTO, DOUBLE, LONG, STRUCT,  Default, Else, Namespace, Switch, Break, Enum, New, Typedef, Case, Float, Return \n";
     std::cout << "Unsigned, Char, For, Short, Sizeof, Const, If, Signed, Static, Continue, Int \n";
 
-    //Задача 4. Съставете С++ конзолна програма, чрез която да изведете на екрана какво е значението на понятията лексема, литерал, команда.
     std::cout << "Ex.4 \n";
     std::cout << "A lexeme is a word, created by rules from a certain language and used in it. \n";
     std::cout << "A literal is a symbolic representation to a specific value of a number, text, char...\n";
